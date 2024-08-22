8612a7ccc146e52cd3076ca5b646230a09e00734
faisal <faisalabdalnasser275@gmail.com>
2024-08-22 22:11:03 +0300

    add home page file

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:weather/features/get_weather/presentation/view/home_page.dart';
+
+void main() {
+  runApp(const Weather());
+}
+
+class Weather extends StatelessWidget {
+  const Weather({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
