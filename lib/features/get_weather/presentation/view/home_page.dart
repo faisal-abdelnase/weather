ced9b8eff6735b0733254ee3e7b9a8bfeade0aa1
faisal <faisalabdalnasser275@gmail.com>
2024-08-22 22:26:10 +0300

    add home page body

--- lib/features/get_weather/presentation/view/home_page.dart
+++ lib/features/get_weather/presentation/view/home_page.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:weather/features/get_weather/presentation/view/search_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text("My Weather", 
+        style: TextStyle(
+          color: Colors.white,
+        ),),
+        actions: [
+          IconButton(
+            onPressed: (){
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => const SearchPage()));
+            },
+            icon: const Icon(
+              Icons.search,
+              color: Colors.white,
+              size: 32,
+              ),
+              ),
+        ],
+        ),
+
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("there is no weather 😔 start", 
+              style: TextStyle(
+                fontSize: 24,
+              ),),
+              Text("searching now 🔍", 
+              style: TextStyle(
+                fontSize: 24,
+              ),),
+            ],
+          ),
+        ),
+    );
   }
 }
