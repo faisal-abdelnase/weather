df39a6a047669b63cc169130cb0db8df13b284f4
faisal <faisalabdalnasser275@gmail.com>
2024-08-23 23:05:19 +0300

    create weather api

---
+++ lib/core/services/weather_api.dart
@@ -0,0 +1,31 @@
+
+import 'package:dio/dio.dart';
+import 'package:weather/features/get_weather/presentation/data/model/weather_model.dart';
+
+class WeatherApi {
+  final Dio dio;
+
+  WeatherApi(this.dio);
+
+  Future<WeatherModel?> getWeather({required String cityName}) async{
+
+    WeatherModel? weather;
+
+    try{
+
+      Response response = 
+      await dio.get("http://api.weatherapi.com/v1/forecast.json?key=d9564fc6a87b4d2f844162959232206&q=$cityName&days=7");
+
+      Map<String, dynamic> jsonData = response.data;
+
+      weather = WeatherModel.fromJson(jsonData);
+
+
+    }catch(e){
+      print(e);
+    }
+
+    return weather;
+
+  }
+}
