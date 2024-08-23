2324854937679d8f87fb88a3c4719b352c98ad6a
faisal <faisalabdalnasser275@gmail.com>
2024-08-23 22:49:45 +0300

    create weather model

--- lib/features/get_weather/presentation/data/model/weather_model.dart
+++ lib/features/get_weather/presentation/data/model/weather_model.dart
@@ -1,3 +1,7 @@
+
+
+import 'package:flutter/material.dart';
+
 class WeatherModel {
   final DateTime date;
   final double temp;
@@ -23,4 +27,54 @@ factory WeatherModel.fromJson(dynamic data){
     minTemp: jsonData['mintemp_c'], 
     weahterStateName: jsonData['condition']['text']);
 }
+
+String getImage(){
+  switch(weahterStateName){
+    case "Moderate rain":
+      return "asset/images/thunderstorm.png";
+    case 'Sleet':
+    case 'Snow':
+    case 'Hail':
+      return "asset/images/snow.png";
+    case "Partly cloudy":
+      return 'asset/images/cloudy.png';
+    case 'Light Rain':
+    case 'Heavy Rain':
+    case 'Showers':
+      return "asset/images/rainy.png";
+    case 'Clear':
+    case 'Light Cloud':
+      return "asset/images/clear.png";
+
+    default: return "asset/images/clear.png";
+  }
+}
+
+
+
+
+MaterialColor getColor(){
+  switch(weahterStateName){
+    case "Moderate rain":
+      return Colors.grey;
+    case 'Sleet':
+    case 'Snow':
+    case 'Hail':
+      return Colors.blue;
+    case "Partly cloudy":
+      return Colors.blueGrey;
+    case 'Light Rain':
+    case 'Heavy Rain':
+    case 'Showers':
+      return Colors.blue;
+    case 'Clear':
+    case 'Light Cloud':
+      return Colors.orange;
+
+    default: return Colors.orange;
+  }
+}
+
+
+
 }
